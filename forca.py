a41066c30dc05c26bd029d5b95e49f2ac2c4d73d
Michael Marin <michael_6-c@hotmail.com>
2023-03-15 22:31:37 -0300

    Quase final.

--- forca.py
+++ forca.py
@@ -11,11 +11,14 @@ def jogar():
         lista_toda = (conteudo[p])
     
     palavra_secreta = str(lista_toda)
+    palavra = lista_toda
+    tentativa = len(palavra)
     enforcou = False
     acertou = False
     letras_acertadas = []
     tamanho_palavra = len(palavra_secreta)
     letras_acer = len(letras_acertadas)
+    letras_faladas = []
 
     for i in range(tamanho_palavra - 1):
         if(letras_acer < tamanho_palavra):
@@ -24,10 +27,12 @@ def jogar():
 
     while(not enforcou and not acertou):
 
+        letras_fim = ("".join(letras_acertadas))
+
         chute = input("\nQual letra? \n")
         chute = chute.strip()
 
-        if(chute.upper() in letras_acertadas):
+        if(chute.upper() in letras_faladas):
             print ('Você já falou essa letra.')
             continue
 
@@ -37,11 +42,29 @@ def jogar():
             if(chute.upper() == letra.upper()):
                 letras_acertadas.pop(index)
                 letras_acertadas.insert(index, chute.upper())
+                letras_faladas.append(chute.upper())
+            else:
+                letras_faladas.append(chute.upper())
             index = index + 1
-            print("Encontrei a letra {}.".format(chute))
-            print(letras_acertadas)
-        
-        print("jogando")       
+
+        letra_h = chute.upper()
+        vez = palavra_secreta.count(chute)
+        tem = vez > 0
+
+        if(tem): 
+            print("Encontrei a letra {} e {} vez.".format(letra_h, vez),"\n",letras_acertadas)
+        else:
+            print("Não tem essa letra.\n",letras_acertadas)
+
+        if(palavra.upper() == letras_fim):
+            print("\nFIM\n")
+            break
+
+        print("jogando")  
+
+    acertou = palavra == palavra_secreta
+    enforcou = tentativa == len(letras_faladas)
+
 
 if(__name__=="__main__"):
     jogar()
