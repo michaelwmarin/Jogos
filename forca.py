c0b876ff4fefa5fd05a341a7ca6c69cc803803f5
Michael Marin <michael_6-c@hotmail.com>
2023-03-16 23:50:15 -0300

    Fim da forca

--- forca.py
+++ forca.py
@@ -10,17 +10,16 @@ def jogar():
         p = random.randrange(0, 245366)
         lista_toda = (conteudo[p])
     
-    palavra_secreta = str(lista_toda)
-    palavra = lista_toda
-    tentativa = len(palavra)
-    enforcou = False
-    acertou = False
+    palavra_secreta = str(lista_toda).strip()
     letras_acertadas = []
+    letras_faladas = []
+    acertou = False
+    enforcou = False
     tamanho_palavra = len(palavra_secreta)
     letras_acer = len(letras_acertadas)
-    letras_faladas = []
 
-    for i in range(tamanho_palavra - 1):
+
+    for i in range(tamanho_palavra):
         if(letras_acer < tamanho_palavra):
             letras_acertadas.append("_")
         continue
@@ -28,24 +27,37 @@ def jogar():
     while(not enforcou and not acertou):
 
         letras_fim = ("".join(letras_acertadas))
+        palavra_fim = palavra_secreta.upper()
+        tentativa = len(letras_faladas)
+        print("JOGANDO\nVOCÊ TEM {} TENTATIVAS.".format(tamanho_palavra-tentativa))
+
+        enforcou = tentativa == tamanho_palavra
+        acertou = letras_fim == palavra_fim
+
+        if(acertou):
+            print("\nFIM\nVocê acertou a palavra\nPARABÉNS!!!!")
+            break
+        elif(enforcou):
+            print("\nFIM\nVocê pedeu não acertou.\nSuas tentativa acabaram\n")
+            break
 
         chute = input("\nQual letra? \n")
-        chute = chute.strip()
+        chute = chute.strip().lower()
 
         if(chute.upper() in letras_faladas):
             print ('Você já falou essa letra.')
             continue
-
-        index = 0
-
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas.pop(index)
-                letras_acertadas.insert(index, chute.upper())
-                letras_faladas.append(chute.upper())
-            else:
-                letras_faladas.append(chute.upper())
-            index = index + 1
+        
+        if(chute in palavra_secreta):
+            letras_faladas.append(chute.upper())
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas.pop(index)
+                    letras_acertadas.insert(index, chute.upper())
+                index += 1
+        else:
+            letras_faladas.append(chute.upper())
 
         letra_h = chute.upper()
         vez = palavra_secreta.count(chute)
@@ -55,16 +67,6 @@ def jogar():
             print("Encontrei a letra {} e {} vez.".format(letra_h, vez),"\n",letras_acertadas)
         else:
             print("Não tem essa letra.\n",letras_acertadas)
-
-        if(palavra.upper() == letras_fim):
-            print("\nFIM\n")
-            break
-
-        print("jogando")  
-
-    acertou = palavra == palavra_secreta
-    enforcou = tentativa == len(letras_faladas)
-
 
 if(__name__=="__main__"):
     jogar()
