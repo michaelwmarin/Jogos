414acfbbe8b9d3a7d2ad5a1c65e1b5f9ffa07f24
Michael Marin <michael_6-c@hotmail.com>
2023-03-17 23:17:37 -0300

    Compreensão de lista

--- forca.py
+++ forca.py
@@ -10,29 +10,40 @@ def jogar():
         p = random.randrange(0, 245366)
         lista_toda = (conteudo[p])
     
-    palavra_secreta = str(lista_toda).strip()
+    palavra_secreta = str(lista_toda).strip().upper()
     letras_acertadas = []
     letras_faladas = []
     acertou = False
     enforcou = False
     tamanho_palavra = len(palavra_secreta)
     letras_acer = len(letras_acertadas)
-
+    numero_tentativas = 0
 
     for i in range(tamanho_palavra):
         if(letras_acer < tamanho_palavra):
             letras_acertadas.append("_")
         continue
+    
+    print("\nQual nível de dificuldade?\n(1) Fácil (2) Médio (3) Difícil\n")
+
+    nivel = int(input("Define o nível: "))
+    facil = nivel == 1
+    medio = nivel == 2
+
+    if(facil):
+        numero_tentativas = 5
+    elif(medio):
+        numero_tentativas = 3
+    else:
+        numero_tentativas = 2
 
     while(not enforcou and not acertou):
 
         letras_fim = ("".join(letras_acertadas))
-        palavra_fim = palavra_secreta.upper()
-        tentativa = len(letras_faladas)
-        print("JOGANDO\nVOCÊ TEM {} TENTATIVAS.".format(tamanho_palavra-tentativa))
+        tentativa = abs(tamanho_palavra+numero_tentativas)
 
         enforcou = tentativa == tamanho_palavra
-        acertou = letras_fim == palavra_fim
+        acertou = letras_fim == palavra_secreta
 
         if(acertou):
             print("\nFIM\nVocê acertou a palavra\nPARABÉNS!!!!")
@@ -41,32 +52,35 @@ def jogar():
             print("\nFIM\nVocê pedeu não acertou.\nSuas tentativa acabaram\n")
             break
 
-        chute = input("\nQual letra? \n")
-        chute = chute.strip().lower()
+        print("JOGANDO\n\nVOCÊ TEM {} TENTATIVAS.".format(tentativa-tamanho_palavra))
 
-        if(chute.upper() in letras_faladas):
-            print ('Você já falou essa letra.')
+        chute = input("\nQual letra? \n")
+        chute = chute.strip().upper()
+
+        if(chute in letras_faladas):
+            print ('\nVocê já falou essa letra.\n')
+            numero_tentativas -= 1 
             continue
         
         if(chute in palavra_secreta):
-            letras_faladas.append(chute.upper())
+            letras_faladas.append(chute)
             index = 0
             for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
+                if(chute == letra):
                     letras_acertadas.pop(index)
-                    letras_acertadas.insert(index, chute.upper())
+                    letras_acertadas.insert(index, chute)
                 index += 1
         else:
-            letras_faladas.append(chute.upper())
+            letras_faladas.append(chute)
 
-        letra_h = chute.upper()
         vez = palavra_secreta.count(chute)
         tem = vez > 0
 
         if(tem): 
-            print("Encontrei a letra {} e {} vez.".format(letra_h, vez),"\n",letras_acertadas)
+            print("\nEncontrei a letra {} e {} vez.\n\n".format(chute, vez),"\n",letras_acertadas)
         else:
-            print("Não tem essa letra.\n",letras_acertadas)
+            print("\nNão tem essa letra.\n\n",letras_acertadas)
+            numero_tentativas -= 1
 
 if(__name__=="__main__"):
     jogar()
